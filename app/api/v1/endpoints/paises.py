81f0eea84265aea79714f8c42e1429c55f18fec8
Higor Camelo <higorcamelo@gmail.com>
2024-05-30 21:42:09 -0300

    Update paises.py

--- app/api/v1/endpoints/paises.py
+++ app/api/v1/endpoints/paises.py
@@ -4,22 +4,13 @@ from app.services.ibge_service import get_countries, get_country_profile, get_co
 router = APIRouter()
 
 @router.get("/countries")
-def list_countries(lang: str = "PT"):
-    try:
-        return get_countries(lang)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def read_countries(lang: str = "PT"):
+    return get_countries(lang)
 
 @router.get("/countries/{country_codes}")
-def get_profile(country_codes: str, lang: str = "PT"):
-    try:
-        return get_country_profile(country_codes, lang)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def read_country_profile(country_codes: str, lang: str = "PT"):
+    return get_country_profile(country_codes, lang)
 
 @router.get("/countries/{country_codes}/indicators/{indicator_ids}")
-def get_indicators(country_codes: str, indicator_ids: str, period: str = None):
-    try:
-        return get_country_indicators(country_codes, indicator_ids, period)
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def read_country_indicators(country_codes: str, indicator_ids: str, period: str = None):
+    return get_country_indicators(country_codes, indicator_ids, period)
